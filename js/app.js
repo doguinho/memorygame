f1b2173bdb406e492bfd08af8bae300da0621ab7
Doug <dougsm@gmail.com>
2018-11-07 16:11:28 -0200

    fix: ignore click on open cards

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var startTime;
 var endTime;
 var stars = document.querySelectorAll(".fa-star");
 
-document.getElementById("restart").addEventListener("click",function(){
+document.getElementById("restart").addEventListener("click", function () {
     restartGame();
     document.getElementById("youWin").classList.toggle("hide");
     document.getElementById("deck").classList.toggle("hide");
@@ -22,62 +22,60 @@ function startGame() {
     matchedCards = 0;
 
     startTime = new Date().getTime();
-    
+
     cardList = shuffle(cardList);
 
-    var moves = document.querySelector(".moves");    
-    moves.innerHTML = counter;    
-    
-    var deck = document.getElementById("deck"); 
+    var moves = document.querySelector(".moves");
+    moves.innerHTML = counter;
+
+    var deck = document.getElementById("deck");
     deck.innerHTML = "";
 
     document.getElementById("starsScore").innerHTML = "";
 
-    for (var s of stars){
+    for (var s of stars) {
         s.classList.remove("hide");
     }
 
     for (var i = 0; i < cardList.length; i++) {
         var card = cardList[i].cloneNode(true);
         clearCard(card);
-        card.addEventListener("click", function(){
-            showCard(this);
-        });
+        card.addEventListener("click", showCard);
         deck.appendChild(card);
     }
 
     var btRestart = document.querySelector(".restart");
-    btRestart.addEventListener("click",function(){
+    btRestart.addEventListener("click", function () {
         restartGame();
     });
 
 }
 
-function restartGame(){
+function restartGame() {
     counter = 0;
-    openCards = [];    
+    openCards = [];
     document.getElementById("score-panel").classList.remove("hide");
     startGame();
 }
 
-function showCard(el){
-    el.classList.add("open","show");
-    addToOpen(el);    
+function showCard(el) {
+    this.classList.add("open", "show");
+    addToOpen(this);
 }
 
-function clearCard(card){
+function clearCard(card) {
     card.classList.remove("match", "open", "show");
 }
 
-function addCounter(){
+function addCounter() {
     counter++;
-    var moves = document.querySelector(".moves"); 
+    var moves = document.querySelector(".moves");
     moves.innerHTML = counter;
 
     hideStars();
 }
 
-function hideStars(){
+function hideStars() {
     if (counter > 15) {
         stars[0].classList.add("hide");
     }
@@ -86,10 +84,10 @@ function hideStars(){
     }
     if (counter > 25) {
         stars[2].classList.add("hide");
-    }    
+    }
 }
 
-function doNotMatch(el,last){
+function doNotMatch(el, last) {
     clearCard(last);
     clearCard(el);
 }
@@ -98,35 +96,43 @@ function matchCards(el, last) {
     el.classList.add("match");
     last.classList.add("match");
     matchedCards++;
+    console.log(cardList.length, matchedCards * 2);
+    el.removeEventListener("click", showCard);
+    last.removeEventListener("click", showCard);
+
     if (cardList.length == matchedCards * 2) {
         setTimeout(function () {
             endGame();
-        },1000);
+        }, 1000);
     }
 }
 
 function addToOpen(el) {
-        
+
     if (openCards.length > 0) {
 
-        var last = openCards[openCards.length - 1];                
+        var last = openCards[openCards.length - 1];
 
-        if (last.dataset.cardName == el.dataset.cardName){
-            matchCards(el,last);
-            openCards = [];
-        } else {
-            openCards.pop();    
-            setTimeout(function () {
-                doNotMatch(el, last);
-            },1000);
-        } 
+        if (el.id != last.id) {
 
-        addCounter();
+            if (last.dataset.cardName == el.dataset.cardName) {
+                matchCards(el, last);
+                openCards = [];
+            } else {
+                openCards.pop();
+                setTimeout(function () {
+                    doNotMatch(el, last);
+                }, 1000);
+            }
+
+            addCounter();
+
+        }
 
     } else {
         openCards.push(el);
     }
-    
+
 }
 
 function elapsedTime() {
@@ -137,12 +143,12 @@ function elapsedTime() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     return `${days} days ${hours}h ${minutes}min ${seconds}s`;
-    
+
 }
 
-function endGame() {    
+function endGame() {
     var deck = document.getElementById("deck");
     deck.classList.toggle("hide");
 
@@ -154,7 +160,7 @@ function endGame() {
     document.getElementById("score-panel").classList.toggle("hide");
 
     var score = document.querySelector(".stars").cloneNode(true);
-    
+
     document.getElementById("starsScore").appendChild(score);
 
 }
